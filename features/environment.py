e3c674bd8ea500c334ca723c7f98733662dbcfa8
automationleg <automationleg@gmail.com>
2022-05-01 23:30:24 +0200

    refactor environment.py

--- features/environment.py
+++ features/environment.py
@@ -6,40 +6,16 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 ALLURE_RESULTS_DIR = os.path.join(ROOT_DIR, 'test_reports')
 
 
-def generate_allure_properties(**env_kwargs):
-    """
-    Create environment.properties file with provided key:value pairs to be displayed in the allure report
-    :param env_kwargs: keyword arguments to include ie: 'ENVIRONMENT':'DEV'
-    :return: None
-    """
-    file_path = os.path.join(ALLURE_RESULTS_DIR, 'environment.properties')
-    with open(file_path, 'a') as properties_file:
-        for key, value in env_kwargs.items():
-            properties_file.write(f'{key}:{value}\n')
-
-
 def before_all(context):
     context.config.setup_logging()
 
-    env_urls = {
-        'DEV': os.environ.get("SITE_URL_DEV"),
-    }
-
-    # set environment to DEV by default in case no environment variable set
-    environment = os.environ.get("ENVIRONMENT", "DEV")
-    context.site_base_url = env_urls[environment]
+    # In general this would normally be defined as constant url per environment instead of env variable
+    context.site_base_url = os.environ.get("SITE_URL")
 
     # authentication
     context.api_key = os.environ.get("API_KEY")
     context.api_sec = os.environ.get("API_SEC")
     context.api_otp = os.environ.get("API_OTP")
-
-    # set allure properties file
-    environment_properties = {
-        'environment': environment,
-        'API URL': context.site_base_url,
-    }
-    # generate_allure_properties(**environment_properties)
 
 
 def before_scenario(context, scenario):
